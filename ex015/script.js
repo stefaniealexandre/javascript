3f47b9d60db3b8231351ab1087e0e1c34c667edc
stefaniealexandre <stefaniealexandre1206@gmail.com>
2025-04-10 21:11:43 -0300

    desfazendo

--- ex015/script.js
+++ ex015/script.js
@@ -15,31 +15,31 @@ function verificar() {
             gênero = 'Homem'
             if (idade >=0 && idade < 10) {
                 //criança
-                img.setAttribute('src', 'foto-bebe-m.jpg')
+                
             } else if (idade < 21) {
                 //jovem
-                img.setAttribute('src', 'foto-jovem-m.jpg')
+               
             } else if (idade < 50) {
                 //adulto
-                img.setAttribute('src', 'foto-adulto-m.jpg')
+               
             } else {
                 //idoso
-                img.setAttribute('src', 'foto-idoso-m.jpg')
+                
             }
         } else if (fsex[1].checked) {
             gênero = 'Mulher'
             if (idade >=0 && idade < 10) {
                 //criança
-                img.setAttribute('src', 'foto-bebe-f.jpg')
+               
             } else if (idade < 21) {
                 //jovem
-                img.setAttribute('src', 'foto-jovem-f.jpg')
+                
             } else if (idade < 50) {
                 //adulto
-                img.setAttribute('src', 'foto-adulto-f.jpg')
+                
             } else {
                 //idoso
-                img.setAttribute('src', 'foto-idoso-f.jpg')
+                
             }
         }
         res.style.textAlign = 'center'
